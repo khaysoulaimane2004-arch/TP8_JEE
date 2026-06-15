13f63b2456847a56f7a99ea0775028f363b97729
khaysoulaimane2004-arch <khaysoulaimane2004@gmail.com>
2026-06-15 20:04:49 +0100

    Add files via upload

---
+++ src/main/java/ma/fst/studentapi/dto/StudentResponseDTO.java
@@ -0,0 +1,13 @@
+package ma.fst.studentapi.dto;
+
+public record StudentResponseDTO(
+
+        Long id,
+        String firstName,
+        String lastName,
+        String email,
+        String major,
+        Integer age
+
+) {
+}
